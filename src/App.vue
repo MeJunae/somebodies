4be665503873eeb23d860143c0cd74015bef8f90
Ivan Favian Elianto <purenaturalism@gmail.com>
2020-10-06 21:35:51 +0700

    feat(main-canvas): render main canvas

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <MainCanvas msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
 <script>
+import MainCanvas from './components/MainCanvas.vue';
+
 export default {
   name: 'App',
+  components: {
+    MainCanvas,
+  },
 };
 </script>
